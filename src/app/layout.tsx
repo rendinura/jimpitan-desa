3b7976692fb3ae64fdd59c84c0cdf114624d6cc6
rendinura <rendinuragustin@gmail.com>
2026-02-02 19:21:04 +0700

    Fix dashboard folder missing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ConvexClientProvider from "./ConvexClientProvider";
 import { Toaster } from "@/components/ui/sonner";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,10 +30,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ConvexClientProvider>
-          {children}
-        <Toaster />
+        <ClerkProvider>
+          <ConvexClientProvider>
+            {children}
+          <Toaster />
         </ConvexClientProvider>
+        </ClerkProvider>
       </body>
     </html>
   );
